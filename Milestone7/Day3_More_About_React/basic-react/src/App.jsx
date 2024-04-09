12c3590d40a4e5f73041c90ce84a149bd3205642
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-04-10 05:14:01 +0600

    React Again

--- Milestone7/Day3_More_About_React/basic-react/src/App.jsx
+++ Milestone7/Day3_More_About_React/basic-react/src/App.jsx
@@ -5,29 +5,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  function handleClick(){
+    alert('button-clicked');
+  }
+  const handleClick2=()=>{
+    alert('Onclick-Done');
+  }
+  const addToFive=(num)=>{
+    alert(num+5);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+      <button onClick={()=>{alert('Third Button');}}>Third Button</button>
+      <button onClick={()=>{alert(()=>{
+        addToFive(5);
+      });}}>4th Button</button>
     </>
   )
 }
