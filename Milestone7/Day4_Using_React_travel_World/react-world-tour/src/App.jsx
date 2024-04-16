b170bfe5ce6e7fc23b39e0d5336721bff262fb2d
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-04-17 04:01:08 +0600

    new-country details with name and flag

---
+++ Milestone7/Day4_Using_React_travel_World/react-world-tour/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Countries from './Countries/Countries'
+
+function App() {
+  return (
+    <>
+      <h1>React World Tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
+
+/**
+ * Basic 6 fundamental of REACT-
+ * 1.Component
+ * 2.JSX
+ *  3.Props
+ * 4.Event handler
+ * 5.State Menagement
+ * 6.useEffect ==> To load Data/ to handle side effects (like using data load with API) 
+ */
