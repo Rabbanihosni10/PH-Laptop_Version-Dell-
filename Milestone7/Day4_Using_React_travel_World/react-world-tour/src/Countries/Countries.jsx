a2c1dfc9f8bc472b9cf39df304952eb94e50aec2
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-04-17 17:58:25 +0600

    Visit a country click on a button and mark it

--- Milestone7/Day4_Using_React_travel_World/react-world-tour/src/Countries/Countries.jsx
+++ Milestone7/Day4_Using_React_travel_World/react-world-tour/src/Countries/Countries.jsx
@@ -5,27 +5,43 @@ import { useState } from "react";
 import Country from "../Country/Country";
 import Noya from "../Noya-Country/Noya";
 import New_Country from "../New-Country/New_Country";
-
+import './Countries.css'
 const Countries = () => {
     const [countries,setCountries]=useState([]);
+    const [visitedCountries,setVisitedCountries]=useState([]);
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountries(data));
     },[])
-
+    const  handleVisitedCountry= country =>{
+        console.log('Add This to your visited country');
+        console.log(country);
+    }
     return (
-        <div>
+        <div className="">
             <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country=> <Country key={country.cca3} country={country}></Country>)
-            }
-            {
-                countries.map(noya=><Noya key={noya.cca3} noya={noya}></Noya>)
-            }
-            {
-                countries.map(new_country=> <New_Country key={new_country.cca3} new_country={new_country}></New_Country>)
-            }
+            <div>
+                <h5>Visited Countries--</h5>
+                <ul></ul>
+            </div>
+            <div className="country-container">
+                {
+                 countries.map(country=> <Country handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>)
+                }
+            </div>
+            <h3>Countries: {countries.length}</h3>
+            <div className="country-container">    
+                {
+                    countries.map(noya=><Noya key={noya.cca3} noya={noya}></Noya>)
+                }
+            </div>
+            <h3>Countries: {countries.length}</h3>
+            <div className="country-container">
+                {
+                    countries.map(new_country=> <New_Country key={new_country.cca3} new_country={new_country}></New_Country>)
+                }
+            </div>
         </div>
     );
 };
--- Milestone7/Day4_Using_React_travel_World/react-world-tour/src/Country/Country.jsx
+++ Milestone7/Day4_Using_React_travel_World/react-world-tour/src/Country/Country.jsx
@@ -1,13 +1,29 @@
 // import React from 'react';
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     console.log(country);
-    const {name,flags}=country;
+    const {name,flags,population,area,cca3,cca2}=country;
+    const [visited,setVisited]=useState(false)
+    const handleVisited=()=>{
+        setVisited(!visited);
+    }
+    console.log(handleVisitedCountry);
     return (
-        <div className="country">
-            <h3>Name:{name.common}</h3>
+        <div className={`country ${visited? 'visited':'non-visted'}` }>
+            <h3 style={{color: visited?'Purple':'Black'}}>Name:{name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Country Code: {cca3}</small></p>
+            <p><small>Country Local Code: {cca2}</small></p>
+            <button>Mark Visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited':'Going'}</button>
+            {
+                visited ? 'I have visited This Country':'I want to visit'
+            }
         </div>
     );
 };
