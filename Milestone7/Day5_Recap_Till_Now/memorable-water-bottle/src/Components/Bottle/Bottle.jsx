3dec6a9295b3e39564ab6f844693a6629aebcb29
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-04-22 03:40:15 +0600

    add btn and event handler

--- Milestone7/Day5_Recap_Till_Now/memorable-water-bottle/src/Components/Bottle/Bottle.jsx
+++ Milestone7/Day5_Recap_Till_Now/memorable-water-bottle/src/Components/Bottle/Bottle.jsx
@@ -1,12 +1,14 @@
 import './Bottle.css'
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle,handleCart}) => {
     console.log(bottle)
-    const {brand,lid_type,capacity_ml}=bottle;
+    const {brand,lid_type,capacity_ml,price}=bottle;
     return (
         <div className="bottle">
             <h3>Bottle :{brand}</h3>
             <h5>Water Capacity: {capacity_ml}</h5>
             <p>Bottle Type: {lid_type}</p>
+            <p><b>Price: $</b>{price}</p>
+            <button onClick={()=>handleCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- Milestone7/Day5_Recap_Till_Now/memorable-water-bottle/src/Components/Bottles/Bottles.jsx
+++ Milestone7/Day5_Recap_Till_Now/memorable-water-bottle/src/Components/Bottles/Bottles.jsx
@@ -5,19 +5,26 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles,setBottles]=useState([])
+    const [cart,setCart]=useState([])
     useEffect(()=>{
         fetch('bottles.json')
         .then(res=>res.json())
         .then(data=>setBottles(data))
     },[])
+    const handleCart=bottle=>{
+        const newCart=[...cart,bottle];
+        setCart(newCart);
+    }
     return (
         <div>
-            <h3>Bottles Here: {bottles.length}</h3>
+            <h3>Bottles Available: {bottles.length}</h3>
+            <h4>Cart: {cart.length}</h4>
             <div className="bottle-container">
                 {
-                    bottles.map(bottle=><Bottle key={bottle.color} bottle={bottle}></Bottle>)
+                    bottles.map(bottle=><Bottle key={bottle.color} bottle={bottle} handleCart={handleCart}></Bottle>)
                 }
             </div>
+
         </div>
     );
 };
