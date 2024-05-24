6270198290952fd9537f23904f63805773febf38
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-05-25 01:58:22 +0600

    blogs proptypes import

--- Milestone7/Day6_knowledge_cafe/react-knowledge-cafe/src/App.jsx
+++ Milestone7/Day6_knowledge_cafe/react-knowledge-cafe/src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Blogs from './components/Blogs/Blogs'
-
+import Bookmarks from './components/Bookmarks/Bookmarks'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl bg-red-300' >Knowledge Cafe</h1>
-      <Header></Header>  
-      <Header></Header>   
-      <Blogs></Blogs> 
+      {/* <h1 className='text-6xl bg-red-300' >Knowledge Cafe</h1> */}
+      <Header></Header>
+      <main className='md:flex'>   
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </main> 
     </>
   )
 }
